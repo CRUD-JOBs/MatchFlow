ad13523d91a89dc3983b8462d60caaf2021477d0
andguz8a <and.guz8a@gmail.com>
2026-02-05 20:00:02 -0500

    Fix activar/inactivar usuario y ajustes de UI

--- Frontend/js/models/Candidate.js
+++ Frontend/js/models/Candidate.js
@@ -1,7 +1,7 @@
-import {updateCandidate, getCandidate} from "./../api.js"
-export default class Candidate{
+import { updateCandidate, getCandidate } from "./../api.js"
+export default class Candidate {
     static _candidates = new Map();
-    constructor(id, name, email, password, isOpen, isAvaiable, jobOffer){
+    constructor(id, name, email, password, isOpen, isAvaiable, jobOffer, phone, city) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -9,79 +9,112 @@ export default class Candidate{
         this.isAvaiable = isAvaiable;
         this.isOpen = isOpen;
         this.jobOffer_id = jobOffer;
+        this.phone = phone;
+        this.city = city;
     }
-    static createCandidate({id = null, name = null, email = null, password = null, isOpen = null, isAvaiable = null, jobOffer = null}){
-        if(!id || !name || !email || !password)return null
-        return new Candidate(id, name, email, password, isOpen || false, isAvaiable || false, jobOffer || null)
+    static createCandidate({
+        id = null,
+        name = null,
+        email = null,
+        password = null,
+        isOpen = null,
+        isAvaiable = null,
+        jobOffer = null,
+        phone = null,
+        city = null
+    }) {
+        if (!id || !name || !email || !password) return null;
+
+        return new Candidate(
+            id,
+            name,
+            email,
+            password,
+            isOpen || false,
+            isAvaiable || false,
+            jobOffer || null,
+            phone,
+            city
+        );
     }
-    static get candidates(){
+    static get candidates() {
         return this._candidates
     }
-    static async getCandidate(id){
-        if(this._candidates.has(id)){
+    static async getCandidate(id) {
+        if (this._candidates.has(id)) {
             return this._candidates.get(id)
         }
-        try{
+        try {
             const candidateProperties = await getCandidate(id);
-            const newCandidate =  this.createCandidate(candidateProperties); 
+            const newCandidate = this.createCandidate(candidateProperties);
             this._candidates.set(id, newCandidate)
             return newCandidate; //returns a promise -> use await
-        } catch(error){
+        } catch (error) {
             console.error("HTTP Error while trying to fetch for candidate", error)
             return null
         }
     }
-    async reserve(jobOffer_id){
-        try{
-            const response = await updateCandidate(this.id, {isAvaiable: false, jobOffer_id: jobOffer_id})
+    async reserve(jobOffer_id) {
+        try {
+            const response = await updateCandidate(this.id, { isAvaiable: false, jobOffer_id: jobOffer_id })
             this.isAvaiable = false
             this.jobOffer_id = jobOffer_id
             return response
-        } catch{
+        } catch {
             console.error("HTTP Error while trying to fetch candidate", error)
             return null
         }
     }
-    async reject(){
-        try{
-            const response = await updateCandidate(this.id, {isAvaiable: true, jobOffer_id: null})
+    async reject() {
+        try {
+            const response = await updateCandidate(this.id, { isAvaiable: true, jobOffer_id: null })
             this.isAvaiable = true
-            this.jobOffer_id= null
+            this.jobOffer_id = null
             return response
-        } catch{
+        } catch {
             console.error("HTTP Error while trying to fetch candidate", error)
             return null
         }
     }
-    async openToWork(){
-        if(this.jobOffer_id !== null){
+    async openToWork() {
+        if (this.jobOffer_id !== null) {
             console.error("Error! candidate can not open to work as it has already an offer")
             return null
         }
-        try{
-            const response = await updateCandidate(this.id, {isOpen:true})
+        try {
+            const response = await updateCandidate(this.id, { isOpen: true })
             this.isOpen = true;
             return response
-        } catch(error){
+        } catch (error) {
             console.error("HTTP Error while trying to fetch candidate", error)
             return null
         }
     }
-    async closeToWork(){
-        try{
-            const response = await updateCandidate(this.id, {isOpen:false})
+    async closeToWork() {
+        try {
+            const response = await updateCandidate(this.id, { isOpen: false })
             this.isOpen = false;
             return response
-        } catch(error){
+        } catch (error) {
             console.error("HTTP Error while trying to fetch candidate", error)
             return null
         }
     }
-    resign(){ //This method requires workd, as the user currently is not being removed from the jobOffer
+    async updateProfile(data) {
+    try {
+        const response = await updateCandidate(this.id, data);
+        Object.assign(this, data);
+        return response;
+    } catch (error) {
+        console.error("Error updating profile", error);
+        return null;
+    }
+}
+    resign() { //This method requires workd, as the user currently is not being removed from the jobOffer
         this.isAvaiable = true; //Sets the property to true
         this.jobOffer = null; //Removes the job offer
-        updateCandidate(this.id, {isAvaiable:true, jobOffer:null}) //Updates the database
+        updateCandidate(this.id, { isAvaiable: true, jobOffer: null }) //Updates the database
         //An update could be creata a resign notice object, send to databse with the jobOfferid, and make the company accept the resignation
     }
-    
+
 }
--- Frontend/js/pages/index.js
+++ Frontend/js/pages/index.js
@@ -1,24 +1,148 @@
-import Candidate from "../models/Candidate.js"
-const renderCandidate = async ()=>{
-    const candidate = Candidate.createCandidate(JSON.parse(sessionStorage.getItem("currentUser")))
-    console.log(candidate)
-    if(candidate===null)return
-    document.querySelector("[data-user-name]").textContent = candidate.name;
-    document.querySelector("[data-user-active]").textContent = candidate.isAvaiable ? "Activo" : "Inactivo";
-    document.querySelector("[data-user-email]").textContent = candidate.email;
+import Candidate from "../models/Candidate.js";
+
+const renderCandidate = async () => {
+    const candidate = Candidate.createCandidate(
+        JSON.parse(sessionStorage.getItem("currentUser"))
+    );
+
+    console.log(candidate);
+
+    if (candidate === null) return;
+
+    const message = document.getElementById("profileMessage");
+
+    const nameEl = document.querySelector("[data-user-name]");
+    const activeBadge = document.querySelector("[data-user-active]");
+    const emailEl = document.querySelector("[data-user-email]");
+    const phoneEl = document.querySelector("[data-user-phone]");
+    const cityEl = document.querySelector("[data-user-city]");
+
+    nameEl.textContent = candidate.name;
+    emailEl.textContent = candidate.email;
+    phoneEl.textContent = candidate.phone || "No registrado";
+    cityEl.textContent = candidate.city || "No registrada";
+
+    activeBadge.textContent = candidate.isOpen ? "Activo" : "Inactivo";
+    activeBadge.className = candidate.isOpen
+        ? "badge bg-success"
+        : "badge bg-secondary";
+
     const btn = document.querySelector("[data-user-work]");
     btn.textContent = candidate.isOpen ? "Desactivar" : "Activar";
-    btn.addEventListener("click", ()=>{
-        if(btn.textContent === "Desactivar"){
-            candidate?.closeToWork()
-            sessionStorage.setItem("currentUser", JSON.stringify(candidate))
+
+    btn.addEventListener("click", async () => {
+        if (candidate.isOpen) {
+            await candidate.closeToWork();
             btn.textContent = "Activar";
-        } else{
-            candidate?.openToWork()
-            btn.textContent = "Desactivar"
-            sessionStorage.setItem("currentUser", JSON.stringify(candidate))
+
+            activeBadge.textContent = "Inactivo";
+            activeBadge.className = "badge bg-secondary";
+        } else {
+            await candidate.openToWork();
+            btn.textContent = "Desactivar";
+
+            activeBadge.textContent = "Activo";
+            activeBadge.className = "badge bg-success";
         }
-    })
+
+        sessionStorage.setItem("currentUser", JSON.stringify(candidate));
+    });
+
+    const editBtn = document.querySelector("[data-edit-profile]");
+    let isEditing = false;
+
+    editBtn.addEventListener("click", () => {
+        if (!isEditing) {
+            convertNameToInput();
+            convertEmailToInput();
+            convertPhoneToInput();
+            convertCityToInput();
+
+            editBtn.textContent = "Guardar cambios";
+            isEditing = true;
+        } else {
+            getEditedValues();
+            restoreProfileView();
+
+            editBtn.textContent = "Editar perfil";
+            isEditing = false;
+
+            
+            message.textContent = "Perfil actualizado correctamente ✅";
+            message.className = "text-success mt-2";
+
+            setTimeout(() => {
+                message.textContent = "";
+            }, 3000);
+        }
+    });
+};
+
+
+
+function convertNameToInput() {
+    convertToInput("[data-user-name]", "text");
 }
 
-document.addEventListener("DOMContentLoaded", renderCandidate)
+function convertEmailToInput() {
+    convertToInput("[data-user-email]", "email");
+}
+
+function convertPhoneToInput() {
+    convertToInput("[data-user-phone]", "tel");
+}
+
+function convertCityToInput() {
+    convertToInput("[data-user-city]", "text");
+}
+
+function convertToInput(selector, type) {
+    const element = document.querySelector(selector);
+    const value = element.textContent.trim();
+
+    const input = document.createElement("input");
+    input.type = type;
+    input.value = value;
+    input.className = "form-control";
+    input.setAttribute(selector.replace("[", "").replace("]", ""), "");
+
+    element.replaceWith(input);
+}
+
+
+function getEditedValues() {
+    const candidate = JSON.parse(sessionStorage.getItem("currentUser"));
+
+    candidate.name = document.querySelector("[data-user-name]").value;
+    candidate.email = document.querySelector("[data-user-email]").value;
+    candidate.phone = document.querySelector("[data-user-phone]").value;
+    candidate.city = document.querySelector("[data-user-city]").value;
+
+    sessionStorage.setItem("currentUser", JSON.stringify(candidate));
+
+    console.log("DATOS EDITADOS:", candidate);
+}
+
+
+function restoreProfileView() {
+    restoreField("[data-user-name]");
+    restoreField("[data-user-email]");
+    restoreField("[data-user-phone]");
+    restoreField("[data-user-city]");
+}
+
+function restoreField(selector) {
+    const input = document.querySelector(selector);
+    if (!input) return;
+
+    const p = document.createElement("p");
+    p.textContent = input.value || "No registrado";
+
+    const attr = selector.replace("[", "").replace("]", "");
+    p.setAttribute(attr, "");
+
+    input.replaceWith(p);
+}
+
+
+document.addEventListener("DOMContentLoaded", renderCandidate);
